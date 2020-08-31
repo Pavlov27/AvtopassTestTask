55b0c013a66e47037b189dab10a432f032482cc8
Nikita Pavlov <npavlovvv@gmail.com>
2020-09-01 01:00:23 +0300

    Initial Commit

---
+++ AvtopassTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvtopassTestTask
+//
+//  Created by Nikita Pavlov on 01.09.2020.
+//  Copyright © 2020 Nikita Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
